5b38bfba3d73b98b1b56e9eae1bb02eefdf596d2
ashutoshgairola <gairola.ashutosh26@gmail.com>
2022-10-16 14:33:31 +0530

    personal drive changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const app = require("./src/app.js");
 
-const port = 4000;
+const port = 80;
 
 const server = http.createServer(app);
 var path = require("path");
 global.appRoot = path.resolve(__dirname);
 
-server.listen(port||'5000', () => {
+server.listen(process.env.PORT||'5000', () => {
   console.log(`Server is live on port ${port}.`);
   console.log(`http://localhost:${port}`);
 });
